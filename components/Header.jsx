cdd668a88851d4f5ee45893c082db59d5527c695
Osuohia Emmanuel <josephe442@gmail.com>
2021-12-07 13:53:47 +0100

    change: bgcolor and padding of the header

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,11 +12,11 @@ const Header = () => {
   }, []);
 
   return (
-    <div className='container mx-auto px-10 mb-8'>
-      <div className="border-b w-full inline-block border-blue-400 py-8">
+    <div className='container mx-auto px-10 mb-5'>
+      <div className="border-b w-full inline-block border-white-400 py-4">
         <div className="md:float-left block">
           <Link href='/'>
-            <span className="cursor-pointer font-bold text-4xl text-white">
+            <span className="cursor-pointer font-bold text-2xl text-white">
               Imaa's Blog
             </span>
           </Link>
